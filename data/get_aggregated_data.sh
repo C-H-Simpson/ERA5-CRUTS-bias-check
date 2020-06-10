770998f545e3dc34f8e0a70fbb7e96a15f2dc03f
Charles Simpson <champs@bas.ac.uk>
2020-06-10 17:34:45 +0100

    add era5 lsm to data download

--- data/get_aggregated_data.sh
+++ data/get_aggregated_data.sh
@@ -1,3 +1,4 @@
 wget http://gws-access.ceda.ac.uk/public/bas_climate/files/champs/bias_explore/era5_t2m_mavg.nc
 wget http://gws-access.ceda.ac.uk/public/bas_climate/files/champs/bias_explore/cruts_tmx_mavg.nc
 wget http://gws-access.ceda.ac.uk/public/bas_climate/files/champs/bias_explore/cruts_tmp_mavg.nc
+wget http://gws-access.ceda.ac.uk/public/bas_climate/files/champs/bias_explore/era5_invariant_lsm.nc
