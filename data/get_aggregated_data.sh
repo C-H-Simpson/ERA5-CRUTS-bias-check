ec2673ec0712c520b9949b9cbcbc9f7f3cf8e5f4
Charles Simpson <champs@bas.ac.uk>
2020-06-10 17:27:23 +0100

    add pre-aggregated data download

---
+++ data/get_aggregated_data.sh
@@ -0,0 +1,3 @@
+wget http://gws-access.ceda.ac.uk/public/bas_climate/files/champs/bias_explore/era5_t2m_mavg.nc
+wget http://gws-access.ceda.ac.uk/public/bas_climate/files/champs/bias_explore/cruts_tmx_mavg.nc
+wget http://gws-access.ceda.ac.uk/public/bas_climate/files/champs/bias_explore/cruts_tmp_mavg.nc
